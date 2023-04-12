d6572432bc517fc71387ec0c1c22eefbca6db42f
sarahp27 <sarah.patel@xloopdigital.com>
2023-04-12 03:48:28 -0700

    Test Case 2 failed

--- src/test/java/com/tdd/CartTest.java
+++ src/test/java/com/tdd/CartTest.java
@@ -15,4 +15,21 @@ public class CartTest {
         //assert
         assertEquals(items, item);
     }
+
+    @Test
+    public void SumOfItems(){
+        //Setup
+        int items=1400;
+        //
+        Cart cart1=new Cart("Meezan Oil",700,2);
+        
+        Cart cart=new Cart();
+        cart.add(cart1);
+        cart.add(cart2);
+        int sum=cart.getTotalSum();
+        //Asset
+        assertEquals(items, sum);
+
+    }
 }
+
